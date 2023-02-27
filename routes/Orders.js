0add736f3b5e1a8e1fbcf9b51afe4b1a8617e21a
kamrulHasan <kamrulhasandhaka704@gmail.com>
2023-02-27 11:03:49 +0600

    my-server

---
+++ routes/Orders.js
@@ -0,0 +1,13 @@
+// const express = require("express");
+// const router = express.Router();
+
+// // middle wares
+// // app.use(cors());
+// // app.use(express.json());
+
+// // orders api 
+
+
+
+
+// module.exports = router;
---
+++ routes/Services.js
@@ -0,0 +1,10 @@
+// const express = require("express");
+// const router = express.Router();
+
+
+// // middle wares
+// // app.use(cors());
+// // app.use(express.json());
+
+
+// module.exports = router;
